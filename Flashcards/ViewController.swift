266c18c62aa5e7ee5becf61c80ed237b5906d8f4
Adélia Manuel <adelia_manuel@hotmail.com>
2018-11-16 21:28:58 -0600

    Lab 3 done and updated read me

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -8,19 +8,44 @@
 
 import UIKit
 
+struct Flashcard {
+    var question: String
+    var answer: String
+}
+
 class ViewController: UIViewController {
     @IBOutlet weak var card: UIView!
     
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
     
+    
+    
     @IBOutlet weak var btnOptionOne: UIButton!
     @IBOutlet weak var BtnOptionTwo: UIButton!
     @IBOutlet weak var BtnOptionThree: UIButton!
     
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    @IBOutlet weak var prevButton: UIButton!
+    
+    //Array to hold our flashcards.
+    var flashcards = [Flashcard]()
+    
+    //Current flashcard index.
+    var currentIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        readSavedFlashcards()
+        if flashcards.count == 0 {
+        updateFlashcard(question: "What's the capital of Brazil?", answer: "Brasilia")
+        } else {updateLabels()
+            updateNextPrevButtons()
+        }
+        
     card.layer.cornerRadius = 20.0
         card.clipsToBounds = true
         card.layer.shadowRadius = 15.0
@@ -50,15 +75,71 @@ class ViewController: UIViewController {
         creationController.flashcardsController = self
     }
     @IBAction func DidTapOnFlashcards(_ sender: Any) {
-        frontLabel.isHidden = false
+        if frontLabel.isHidden == true { frontLabel.isHidden = false}
+        else {
+            frontLabel.isHidden = true
+        }
     }
-   
+    func updateLabels(){
+        //get current flashcard
+        let currentFlashcard = flashcards[currentIndex]
+        //update labels
+        frontLabel.text = currentFlashcard.question
+        backLabel.text = currentFlashcard.answer
+    }
     func updateFlashcard(question: String, answer: String)  {
         
-        dismiss(animated:true)
+        let flashcard=Flashcard(question: question, answer: answer)
+       
+        frontLabel.text = flashcard.question
+        backLabel.text = flashcard.answer
+       
+        flashcards.append(flashcard)
         
+        //logging to the console
+        print ("😎 Added new flashcard ")
+        print("😎 We now have \(flashcards.count) flashcards")
+        // Update current index
+        currentIndex = flashcards.count - 1
+        print("😎 Our current index is \(currentIndex)")
         
+        // update buttons
+        updateNextPrevButtons()
+        
+        //update labels
+        updateLabels()
+       
     }
+    func updateNextPrevButtons(){
+        //Disable next button if at the end
+        if currentIndex == flashcards.count - 1{
+        nextButton.isEnabled = false
+        } else {
+            nextButton.isEnabled = true
+        }
+        // Disable prev button if at the beginning
+    }
+    
+    func saveAllFlashcardsToDisk(){
+        
+        
+        // from flashcard array to dictionary array
+        let dictionaryArray = flashcards.map {(card) -> [String: String] in return ["question": card.question, "answer": card.answer]
+        }
+            //save array on disk using userDefaults
+            UserDefaults.standard.set(dictionaryArray, forKey: "flashcards")
+        //log in
+        print("🎉 Flashcards saved to Userdefaults")
+    }
+    func readSavedFlashcards(){
+        if let dictionaryArray = UserDefaults.standard.array(forKey: "flashcards") as? [[String: String]]
+    {        let savedCards = dictionaryArray.map { dictionary -> Flashcard in return Flashcard(question: dictionary ["question"]!, answer: dictionary ["answer"]!)
+            
+        }
+        flashcards.append(contentsOf: savedCards)
+        }
+    }
+    
     @IBAction func DidTapOptionOne(_ sender: Any) {
     btnOptionOne.isHidden = true
     }
@@ -70,5 +151,27 @@ class ViewController: UIViewController {
     @IBAction func DidTapOptionThree(_ sender: Any) {
     BtnOptionThree.isHidden = true
     }
+    
+    @IBAction func DipTapOnNext(_ sender: Any) {
+        // Increase current index
+        currentIndex = currentIndex + 1
+        // update labels
+        updateLabels()
+        
+        //uddate buttons
+        updateNextPrevButtons()
+    }
+    
+    @IBAction func DipTapOnPrev(_ sender: Any) {
+        // Increase current index
+        currentIndex = currentIndex - 1
+        // update labels
+        updateLabels()
+        
+        //uddate buttons
+        updateNextPrevButtons()
+    }
+    
+    
 }
 
---
+++ Flashcards/readme.md3.txt
@@ -0,0 +1,18 @@
+
+## Lab 3
+
+### App Description
+An app that can browse through multiple flashcard, can change questions, and can see the previously created flashcards.
+### App Walk-though
+
+<img src="http://g.recordit.co/Gnt4liybe9.gif" width=200><br>
+
+`TODO://` In the User stories section below, add an `x` between the `-[ ]` 
+## Required
+- [x] User can browse through multiple flashcards
+- [x] User can re-open the app and see previously created flashcards
+- [x] Push code to GitHub
+## Optional
+- [ ] User can delete a flashcard
+- [ ] User can edit existing flashcard
+- [ ] User can store multiple choice questions
